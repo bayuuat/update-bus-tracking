4544106e75017dfa830ca16fd7a73fe05415f409
naofal <naofal.nirvana@yahoo.com>
2022-01-21 15:06:36 +0700

    update styles

--- application/views/bus/booking-detail.php
+++ application/views/bus/booking-detail.php
@@ -1,6 +1,6 @@
 <div class="booking container-fluid">
     <div class="row h-100">
-        <div class="col">
+        <div class="col pl-5">
             <div class="main-title row my-3 ml-3">
                 <button class="btn rounded-button" type="submit">
                     <i class="fa fa-arrow-left" aria-hidden="true"></i>
@@ -8,62 +8,65 @@
                 <h2>Charging Station Bookings</h2>
             </div>
             <div class="row">
-                <div class="timeline card col-2 mx-3">
+                <div class="timeline card col-3 mx-3">
                     <div class="justify-content">
                         <p>Your Booked Station</p>
                         <hr>
                     </div>
                 </div>
-                <div class="main-content col">
-                    <div class="row">
-                        <div class="card-horizontal">
-                            <div class="img-square-wrapper">
-                                <img class=""
-                                    src="https://asset.kompas.com/crops/gaH-dtbdYFUEn76HSO-jOXp0MKU=/0x0:1280x853/750x500/data/photo/2021/08/10/61120c444598c.jpeg"
-                                    alt="Card image cap">
+                <div class="main-content col px-5">
+                    <div class="card-horizontal">
+                        <div class="row">
+                            <div class="col-auto">
+                                <div class="img-square-wrapper my-auto">
+                                    <img class=""
+                                        src="https://asset.kompas.com/crops/gaH-dtbdYFUEn76HSO-jOXp0MKU=/0x0:1280x853/750x500/data/photo/2021/08/10/61120c444598c.jpeg"
+                                        alt="Card image cap">
+                                </div>
                             </div>
-                            <div class="card-body">
-                                <div class="row row-cols-1">
-                                    <h3 class="">FTEIC Charging Station</h3>
-                                    <p class="">2 KM AWAY</p>
-                                </div>
-                                <div class="row row-cols-1">
-                                    <h5 class="">Booked</h5>
-                                    <p>You already book @31-01-2022 07:00 AM</p>
-                                </div>
-                                <div class="row row-cols-1">
-                                    <h5 class="">Location</h5>
-                                    <p>Parkiran Dosen Gedung AJ FTEIC</p>
-                                </div>
-                                <div class="row row-cols-1">
-                                    <h5 class="">Status</h5>
-                                    <p>Available</p>
+                            <div class="col">
+                                <div class="card-body d-flex align-items-start flex-column h-100">
+                                    <div class="judul">
+                                        <h3>FTEIC Charging Station</h3>
+                                        <p>2 KM AWAY</p>
+                                    </div>
+                                    <div class="konten mt-auto">
+                                        <div class="konten-1">
+                                            <h5 class="">Booked</h5>
+                                            <p class="status-book">You already book @31-01-2022 07:00 AM</p>
+                                        </div>
+                                        <div class="konten-2">
+                                            <h5 class="">Location</h5>
+                                            <p class="lokasi">Parkiran Dosen Gedung AJ FTEIC</p>
+                                        </div>
+                                        <div class="konten-3">
+                                            <h5 class="">Status</h5>
+                                            <p class="status-station">Available</p>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div class="books-detail row my-4">
-                        <div class="col-9">
-                            <div class="tombol-buatan my-3">
-                                <i class="fas fa-charging-station"></i>
-                                <p>Charging Station</p>
-
+                    <div class="books-detail">
+                        <div class="row my-4">
+                            <div class="col-9">
+                                <div class="tombol-buatan row my-3">
+                                    <i class="fas fa-charging-station"></i>
+                                    <p>Charging Station</p>
+                                </div>
+                                <div class="tombol-buatan row my-3">
+                                    <i class="fas fa-charging-station"></i>
+                                    <p>Monday, 31 January 2022</p>
+                                </div>
+                                <div class="tombol-buatan row my-3">
+                                    <p><i class="fas fa-charging-station"></i>05:00 AM</p>
+                                </div>
                             </div>
-                            <div class="tombol-buatan my-3">
-                                <i class="fas fa-charging-station"></i>
-                                <p>Monday, 31 January 2022</p>
-
-                            </div>
-                            <div class="tombol-buatan my-3">
-
-                                <p><i class="fas fa-charging-station"></i>05:00 AM</p>
-                            </div>
-                        </div>
-
-                        <div class="col-3">
-                            <div class="mt-auto">
-                                <p>Book Now</p>
-
+                            <div class="col-3 align-items-end d-flex">
+                                <div class="tombol-buatan row my-3">
+                                    <p class="book-now my-auto">BOOK NOW</p>
+                                </div>
                             </div>
                         </div>
                     </div>
